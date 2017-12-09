fe3ac084ac99d1f92eef238fe5f7b4423076c353
Zenarii <AlfieABaxter@Yahoo.com>
2017-12-09 11:33:02 +0000

    Player has full movement

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ class Entity(pygame.sprite.Sprite):
 
     def __init__(self):
         #Any position in width/length, but a multiple of 30 to fit grid
-        self.position = choice(range(0,width,30)),choice(range(0,height,30))
+        self.position = [choice(range(0,width,30)),choice(range(0,height,30))]
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface(tileSize)
         self.image.fill((0,0,0))
@@ -15,9 +15,14 @@ class Entity(pygame.sprite.Sprite):
         self.rect.x = self.position[0]
         self.rect.y = self.position[1]
 
-
+    def move(self, direction):
+        if direction == "up": self.position[1] -= tileSize[1]; player.rect.y = player.position[1]
+        elif direction == "left": self.position[0] -= tileSize[0]; player.rect.x = player.position[0]
+        elif direction == "down": self.position[1] += tileSize[1]; player.rect.x = player.position[1]
+        elif direction == "right": self.position[0] += tileSize[0]; player.rect.x = player.position[0]
 
 def draw():
+    screen.fill((0,255,0))
     screen.blit(player.image,player.position)
     pygame.display.flip()
 
@@ -30,55 +35,19 @@ pygame.display.flip()
 
 player = Entity()
 
-while True:
+upKey = pygame.K_w
+leftKey = pygame.K_a
+downKey = pygame.K_s
+rightKey = pygame.K_d
+
+while True: #Game loop
     for event in pygame.event.get():
+        pressed = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             sys.exit()
+        if pressed[upKey]: player.move("up")
+        elif pressed[leftKey]: player.move("left")
+        elif pressed[downKey]: player.move("down")
+        elif pressed[rightKey]: player.move("right")
 
     draw()
-
-
-"""
-#map ="""########
-#      #
-#      #
-#      #
-#      #
-########
-"""
-
-#width = 9
-#height = 6
-#size = ((width-1)*30,height*30)
-#screen = pygame.display.set_mode(size)
-
-player = Player()
-
-map = map[0:player.position] + "@" + map[player.position+1:len(map)]
-print(map)
-
-
-map = map[0:player.position] + "@" + map[player.position+1:len(map)]
-print(map)
-map = map.replace("@"," ")
-
-player.move("up")
-map = map[0:player.position] + "@" + map[player.position+1:len(map)]
-print(map)
-map = map.replace("@"," ")
-
-player.move("up")
-map = map[0:player.position] + "@" + map[player.position+1:len(map)]
-print(map)
-map = map.replace("@"," ")
-
-player.move("right")
-map = map[0:player.position] + "@" + map[player.position+1:len(map)]
-print(map)
-map = map.replace("@"," ")
-
-player.move("right")
-map = map[0:player.position] + "@" + map[player.position+1:len(map)]
-print(map)
-map = map.replace("@"," ")
-"""
