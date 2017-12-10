0a0b13b1bd536223757f582f4f10330e27b40204
Zenarii <AlfieABaxter@Yahoo.com>
2017-12-10 10:59:11 +0000

    trolls move but don't have collision detection

--- game.py
+++ game.py
@@ -2,18 +2,18 @@ from random import randint, choice
 import pygame, sys
 
 class Entity(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, color):
         #Any position in width/length, but a multiple of 30 to fit grid
 
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface(tileSize)
-        self.image.fill((0,0,0))
+        self.image.fill(color)
 
         self.rect = self.image.get_rect()
 
         positionNotSet=True
         while positionNotSet:
-            self.position=[choice(range(0,width,30)),choice(range(0,height,30))]
+            self.position=[choice(range(0,width,16)),choice(range(0,height,16))]
             self.rect.x = self.position[0]
             self.rect.y = self.position[1]
             for wall in walls:
@@ -59,6 +59,12 @@ class Entity(pygame.sprite.Sprite):
                     self.rect.x = self.position[0]
                     break
 
+class Troll(Entity, pygame.sprite.Sprite):
+    def __init__(self, color):
+        Entity.__init__(self,color)
+        pygame.sprite.Sprite.__init__(self)
+
+
 
 class Block(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -87,16 +93,19 @@ def draw():
 
     screen.blit(player.image,player.position)
     for wall in walls: screen.blit(wall.image, wall.position)
-
+    for troll in trolls: screen.blit(troll.image, troll.position)
     pygame.display.flip()
 
 
-tileSize = (30,30)
-size = width, height = 810, 600
+tileSize = (16,16)
+size = width, height = int(tileSize[0] * 27), int(tileSize[1] * 20)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Escape the trolls!")
 screen.fill((0,0,255))
 pygame.display.flip()
+
+BLUE = (10,10,255)
+RED = (255,10,10)
 
 upKey = pygame.K_w
 leftKey = pygame.K_a
@@ -118,16 +127,30 @@ for i in range(0,height,tileSize[1]):
     walls.append(wall)
 #----[End Wall Generation]------------------------------------------------------
 
-player = Entity()
+player = Entity(BLUE)
+troll1 = Troll(RED)
+troll1.position = [16,16]
+troll2 = Troll(RED)
+troll2.position = [64,16]
+trolls = [troll1,troll2]
+
+print("----[NEW RUN]----")
 
 while True: #Game loop
     for event in pygame.event.get():
+        playerInput = False
         pressed = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             sys.exit()
-        if pressed[upKey]: player.move("up")
-        elif pressed[leftKey]: player.move("left")
-        elif pressed[downKey]: player.move("down")
-        elif pressed[rightKey]: player.move("right")
+
+        if pressed[upKey]: player.move("up"); playerInput = True
+        elif pressed[leftKey]: player.move("left");  playerInput = True
+        elif pressed[downKey]: player.move("down");  playerInput = True
+        elif pressed[rightKey]: player.move("right");  playerInput = True
+
+    if playerInput:
+        for troll in trolls:
+            troll.move(choice(["left","right","up","down"]))
+        playerInput = False
 
     draw()
